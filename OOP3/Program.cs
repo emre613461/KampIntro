0e18b551f543054d7953e0e97c472a98a846b40f
EMRE KOÇ <56259918+emre613461@users.noreply.github.com>
2021-03-06 14:34:20 +0300

    06.03

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           IKrediManager ihtiyaçKrediManager = new İhtiyaçKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafkredi = new EsnafKredi();
+            ILoggerService databasLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLogerService = new FileLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafkredi, new SmsLogger());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyaçKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
